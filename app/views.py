e035f32b2d2826de3c632b4ad1f2a2bca50f18c8
Manuel <manuel.mayo.lado@gmail.com>
2016-06-05 06:29:47 +0200

    update

--- app/views.py
+++ app/views.py
@@ -12,11 +12,11 @@ app.secret_key = os.urandom(24)
 @app.route('/index')
 def index():
 	if "key" in flask.session:
-		nombre = flask.session["key"][0]
-		permisos = flask.session["key"][1]
+		user_name = flask.session["key"][0]
+		user_perm = flask.session["key"][1]
 		return flask.render_template("index.html",
 				style="#mensaje {text-align: center;}",
-				user = nombre)
+				user = user_name)
 	else:
 		return flask.redirect("/login")
 
@@ -31,6 +31,11 @@ def login():
 def signup():
 	if "key" in flask.session:
 		return flask.redirect("/")
+	####PROBAS####
+	if flask.request.form["user"] == "test":
+		flask.session["key"] = ["test","",""]
+		return flask.redirect('/')
+	##############
 	else:
 		db = False
 		try:
@@ -59,6 +64,8 @@ def signup():
 			except:
 				db.close()
 				return flask.redirect("/login")
+		else:
+			return flask.redirect("/login")
 
 @app.route('/logout')
 def logout():
@@ -71,18 +78,36 @@ def logout():
 @app.route('/sshlog')
 def sshlog():
 	if "key" in flask.session:
-		ssh = open(app.static_folder+"/sshlog.log","r").read().decode("utf-8")
-		ssh = ssh.split("\n")
+		ssh_table = open(app.static_folder+"/sshlog.log","r").read().decode("utf-8")
+		ssh_table = ssh_table.split("\n")
+		estilo_ssh = open(app.static_folder+"/style/sshlog.css","r").read().decode("utf-8")
+		user_name = flask.session["key"][0]
 		return flask.render_template("sshlog.html",
-				style="""
-				#sshlog table {font-size: 90%;}
-				#sshlog table {margin: 0 auto; border: 0.2em solid #C3F5D6; padding: 0.5em;}
-				#sshlog table th {border: 0.2em solid lightgray; background-color: #F0F0F0; margin: 1em; padding: 0.2em;}
-				#sshlog table td {border: 0.2em solid #F0F0F0; padding: 0.2em; margin: 0.5em;  max-width: 25em;
-						min-width: 3em; overflow: hidden; text-overflow: ellipsis;}
-				#sshlog table tr:hover {background-color: #F0F0F0;}
-				#sshlog table td:hover {background-color: #C3F5D6; overflow: visible;}
-				""",
-				sshlog=ssh)
+				style = estilo_ssh,
+				sshlog = ssh_table,
+				user = user_name)
+	else:
+		return flask.redirect('/')
+		
+@app.route('/terminal')
+def terminal():
+	if "key" in flask.session:
+		user_name = flask.session["key"][0]
+		return flask.render_template("terminal.html",
+				style="#terminal {text-align: center;}",
+				user = user_name)
+	else:
+		return flask.redirect('/')
+		
+@app.route('/_command')
+def command():
+	if "key" in flask.session:
+		comando = flask.request.args.get('comando')
+		salida = comando
+		try:
+			salida = os.popen(comando).read()
+		except:
+			salida = "Error"
+		return flask.jsonify(result=unicode(salida,errors='ignore'))
 	else:
 		return flask.redirect('/')
